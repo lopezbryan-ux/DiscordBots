5423bce9d8fb3256312ed2bdbc1a049cda433497
Bryan Lopez <lopez_bryan@rocketmail.com>
2025-09-10 20:37:50 -0500

    Remove server.ts and users.ts

--- WarhammerBot/src/commands/utility/server.ts
+++
@@ -1,8 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-
-export const server = {
-  data: new SlashCommandBuilder().setName('server').setDescription('Provides information about the server.'),
-  async execute(interaction: any) {
-    await interaction.reply(`Server name: ${interaction.guild?.name}\nTotal members: ${interaction.guild?.memberCount}`);
-  },
-};
