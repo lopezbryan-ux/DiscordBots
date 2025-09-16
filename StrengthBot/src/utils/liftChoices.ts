35a27c38523d145f88de037b4fe46c7203788e93
AdrianFlores1441 <69173579+AdrianFlores1441@users.noreply.github.com>
2025-09-15 22:17:11 -0500

    added sum lifs
    above

--- StrengthBot/src/utils/liftChoices.ts
+++ StrengthBot/src/utils/liftChoices.ts
@@ -6,10 +6,14 @@ export const CompoundLifts = [
   { name: 'Barbell Deadlift', value: 'Barbell Deadlift' },
   { name: 'Chin Up (BW + Weights)', value: 'Chin Up (BW + Weights)' },
   { name: 'Barbell Standing OHP', value: 'Barbell Standing OHP' },
+  { name: 'Strict Curl', value: 'Strict Curl' },
 ];
 
 export const ArmWrestlingLifts = [
   { name: 'Side Pressure (Wrist wrench)', value: 'Side Pressure (Wrist wrench)' },
   { name: 'Static Pronation (Standing)', value: 'Static Pronation (Standing)' },
   { name: 'Static Riser (Standing)', value: 'Static Riser (Standing)' },
+  { name: 'Partial Preacher Curl', value: 'Partial Preacher Curl' },
+  { name: 'Wrist Curl', value: 'Wrist Curl' },
+  { name: 'Supinating Press', value: 'Supinating Press' },
 ];
