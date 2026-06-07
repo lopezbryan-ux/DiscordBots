589977860f83a9d03771c99c78cc1431637aae89
Bryan Lopez <lopez_bryan@rocketmail.com>
2026-06-07 15:57:34 -0500

    refactor(deploy-commands): add eslint directive to suppress TypeScript any type warning

--- StrengthBot/src/deploy-commands.ts
+++ StrengthBot/src/deploy-commands.ts
@@ -10,6 +10,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const commands: any[] = [];
 
 function getAllCommandFiles(dir: string): string[] {
